3686ff7bb793ebf4395cda5347e257c658ee0093
xbound <54498130+xbound@users.noreply.github.com>
2024-01-24 01:50:03 +0800

    Add files via upload

--- src/main/java/net/wurstclient/commands/GSLaunchCmd.java
+++ src/main/java/net/wurstclient/commands/GSLaunchCmd.java
@@ -1,1 +1,45 @@
+/*
+ * Copyright (c) 2014-2023 Wurst-Imperium and contributors.
+ *
+ * This source code is subject to the terms of the GNU General Public
+ * License, version 3. If a copy of the GPL was not distributed with this
+ * file, You can obtain one at: https://www.gnu.org/licenses/gpl-3.0.txt
+ */
+package net.wurstclient.commands;
 
+import net.minecraft.item.ItemStack;
+import net.minecraft.registry.Registries;
+import net.wurstclient.command.CmdError;
+import net.wurstclient.command.CmdException;
+import net.wurstclient.command.CmdSyntaxError;
+import net.wurstclient.command.Command;
+import net.wurstclient.util.ChatUtils;
+
+public final class GSLaunchCmd extends Command
+{
+	public GSLaunchCmd()
+	{
+		super("gslaunch", "launch the GhastSimulator.", ".gslaunch");
+	}
+	
+	@Override
+	public void call(String[] args) throws CmdException
+	{
+		
+		if(args.length > 1)
+			throw new CmdSyntaxError();
+		WURST.getHax().ghastSimulatorHack.launch();
+	}
+		
+	@Override
+	public String getPrimaryAction()
+	{
+		return "Launch the GhastSimulator.";
+	}
+	
+	@Override
+	public void doPrimaryAction()
+	{
+		WURST.getCmdProcessor().process("gslaunch");
+	}
+}
